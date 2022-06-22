efb90ac2e1e20c03668a49f3fde1c26a340965af
Jade Franklin <jaylan.franklin09@gmail.com>
2022-06-22 14:46:09 -0500

    Initial commit

---
+++ commandss/start.py
@@ -0,0 +1,57 @@
+# <----------------------------------Imports---------------------------------------->
+import discord
+from discord.ext import commands
+import random
+import time
+# <-----------------------------------Bot--------------------------------------------->
+bot = commands.Bot(command_prefix='-', case_insensitive=True)
+
+
+class Trans(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def divide(self, ctx, a: int, b: int):
+        await ctx.send(ctx.channel.id)
+        await ctx.send(f"If you put {a} into {b} groups, you get {a / b} in each group.")
+
+
+    @commands.command()
+    async def test(ctx, name, pronouns):
+        ll = ""
+        ll2 = []
+        for i in pronouns:
+            if i == "/":
+                ll2.append(ll)
+                ll = ""
+            else:
+                ll += i
+        ll2.append(ll)
+        t_choices1 = [f"{name} is a cool person, {ll2[0]} is a great friend of mine, I love {ll2[1].lower()}!",
+                      f"Have you heard from {name}? We're going to the mall with {ll2[1].lower()} today! Let me know when "
+                      f"{ll2[0].lower()} pops up!",
+                      f"{name} is too cool for school 😎. {ll2[0]} is so amazing. One reason I enjoy being around "
+                      f"{ll2[1].lower()} is because we have great conversations.",
+                      ]
+
+        await ctx.send("Processing...", delete_after=2)
+        time.sleep(3)
+        await ctx.send(random.choice(t_choices1))
+
+    @commands.command()
+    async def embed(ctx):
+        lol = await ctx.send("What should the title be? ")
+        title = await bot.wait_for("message", timeout=30)
+        await lol.delete()
+        lol2 = await ctx.send("What should the message say?")
+        message = await bot.wait_for('message', timeout=300)
+        await lol2.delete()
+        final = ""
+        for i in message.content:
+            if i == "\n":
+                final += "\n"
+            else:
+                final += i
+        embed = discord.Embed(title=title.content, description=final)
+        await ctx.send(embed=embed)
---
+++ main.py
@@ -0,0 +1,54 @@
+# <----------------------------------Imports---------------------------------------->
+import discord
+from discord.ext import commands
+from commandss import start as s
+# <-----------------------------------Bot--------------------------------------------->
+# Create a bot
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('-'), case_insensitive=True)
+
+
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is online!")
+
+
+@bot.command()
+async def taco(ctx):
+    await ctx.message.mentions[0].edit(nick="Taco")
+    await ctx.send(f"{ctx.message.mentions[0].name}'s name has been changed to taco lol")
+
+
+@bot.command()
+async def nickname(ctx, nick):
+    l = ["<", "@", "#", ">"]
+    if PermissionError:
+        await ctx.send("Cannot change Owners nickname")
+    for i in l:
+        if i in nick:
+            await ctx.send("Nickname cannot contain these symbols: **<, @, #, >**")
+            return
+    if nick == "":
+        await ctx.send("Nickname is empty")
+    elif len(nick) > 32:
+        await ctx.send("Nickname is too long")
+    elif len(nick) < 2:
+        await ctx.send("Nickname is too short")
+    else:
+        await ctx.message.author.edit(nick=nick)
+        await ctx.send(f"{ctx.message.author.name}'s nickname has been changed to {nick}")
+
+
+@bot.command()
+async def clear(ctx, amount=5):
+    await ctx.message.delete()
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f"{amount} messages deleted", delete_after=5)
+
+
+
+def run():
+    bot.add_cog(s.Trans(bot))
+    bot.run("OTg0NjI2OTYyMTcxNTY4MTg4.GJ-x80.0nP6els4BN9tZNC5e6dMiYXl1H0mLu04I-7QRg")
+run()
